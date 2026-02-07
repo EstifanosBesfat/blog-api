3b930a715dcaa5e78580193a589d84c2491e0160
EstifanosBesfat <besfatestifanos@gmail.com>
2026-02-07 11:05:51 -0600

    feat: implement login logic

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const { createUser, findEmail } = require("../repositories/userRepository");
+const { generateToken } = require("../utils/jwtHelper");
 
 // register new user
 const registerUser = async (user) => {
@@ -13,8 +14,33 @@ const registerUser = async (user) => {
   // hash password
   const hashedPassword = await bcrypt.hash(password, 10);
   // create user
-  newUser = await createUser({ username, email, password: hashedPassword });
+  const newUser = await createUser({
+    username,
+    email,
+    password: hashedPassword,
+  });
   return newUser;
 };
 
-module.exports = { registerUser };
+// login existing user
+const loginUser = async ({ email, password }) => {
+  // check if user is found
+  const matchUser = await findEmail(email);
+  if (!matchUser) {
+    // don't use 'user not found' it helps hackers
+    throw new Error("Invalid credentials");
+  }
+
+  // compare password
+  const isMatch = await bcrypt.compare(password, matchUser.password);
+  if (!isMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  // generate token to make the login process easier for future
+  const token = generateToken(matchUser);
+
+  return { token, matchUser };
+};
+
+module.exports = { registerUser, loginUser };
