feefe6d7845ba22368ae8a093ada4ed56103bc52
EstifanosBesfat <besfatestifanos@gmail.com>
2026-02-07 02:45:24 -0600

    feat: add register controler

---
+++ src/controllers/authController.js
@@ -0,0 +1,18 @@
+const { registerUser } = require("../services/authService");
+
+const register = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  // valdiate input (bouncer)
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "Username, email, and password are required" });
+  }
+  try {
+    const user = await registerUser({username, password, email});
+    return res.status(201).json({ message: "user registered!" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {register}
