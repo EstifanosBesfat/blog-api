dd6aa9e1914e447f92b15a0b631c23ab2f82f49f
EstifanosBesfat <besfatestifanos@gmail.com>
2026-02-08 11:27:56 -0600

    fix: prevent middleware crash when authorization header is missing

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,13 +4,13 @@ const jwt = require("jsonwebtoken");
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
+  // get token it usally looks like " Bearer <token>" so we need to split the string
+  const token = authHeader && authHeader.split(" ")[1];
+
   // check if the token exists
   if (!token) {
     return res.status(401).json({ error: "Unauthoraized" });
   }
-
-  // get token it usally looks like " Bearer <token>" so we need to split the string
-  const token = authHeader.split(" ")[1];
 
   // verify token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
