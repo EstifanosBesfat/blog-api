50bde636385cea0fe563a5a1a042f83cce2b58d0
EstifanosBesfat <besfatestifanos@gmail.com>
2026-02-08 11:14:13 -0600

    feat(routes): add authenticated profile route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { authenticateToken } = require("../middlewares/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/profile", authenticateToken, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.user });
+});
 
 module.exports = router;
