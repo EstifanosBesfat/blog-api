46a56b5907edef85d8e2084a6b1928cccf3d3fe2
EstifanosBesfat <besfatestifanos@gmail.com>
2026-02-09 00:31:04 -0600

    feat(posts): add find, update, and delete methods with ownership checks

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -15,4 +15,22 @@ const findAllPosts = async () => {
   return result.rows; // Return the array of posts
 };
 
-module.exports = { findAllPosts, createPost };
+const findPostById = async (id) => {
+    const result = await db.query("SELECT * FROM posts WHERE id = $1", [id]);
+    return result.rows[0];
+};
+
+const updatePost = async (id, title, content) => {
+    const result = await db.query(
+        "UPDATE posts SET title = $1, content = $2, updated_at = NOW() WHERE id = $3 RETURNING *",
+        [title, content, id]
+    );
+    return result.rows[0];
+};
+
+const deletePost = async (id) => {
+    await db.query("DELETE FROM posts WHERE id = $1", [id]);
+    return true;
+};
+
+module.exports = { findAllPosts, createPost, deletePost, findPostById, updatePost };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,10 @@
-const { createPost, findAllPosts } = require("../repositories/postRepository");
+const {
+  createPost,
+  findAllPosts,
+  findPostById,
+  updatePost,
+  deletePost,
+} = require("../repositories/postRepository");
 
 const createNewPost = async ({ title, content, userId }) => {
   const post = await createPost({ title, content, userId });
@@ -10,4 +16,29 @@ const getAllPosts = async () => {
   return posts;
 };
 
-module.exports = { createNewPost, getAllPosts };
+const updatePostById = async (postId, userId, title, content) => {
+  const post = await findPostById;
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  // ownership check
+  if (post.user_id !== userId) {
+    throw new Error("Access Denied: You are not the author");
+  }
+
+  return await updatePost(postId, title, content);
+};
+
+const deletePostById = async (postId, userId) => {
+    const post = await findPostById(postId);
+    if (!post) throw new Error("Post not found");
+
+    if (post.user_id !== userId) {
+        throw new Error("Access Denied: You are not the author");
+    }
+
+    return await deletePost(postId);
+};
+
+module.exports = { createNewPost, getAllPosts, updatePostById, deletePostById };
